ac6cc6f5dd40776c78b70d075d72de055840f244
saddansyah <saddanakbar@gmail.com>
2023-07-21 09:43:59 +0700

    fix: CORS

--- index.js
+++ index.js
@@ -30,12 +30,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.setHeader('Access-Control-Allow-Methods', '*');
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 // Use route
 app.use('/api/books', booksRoute);
